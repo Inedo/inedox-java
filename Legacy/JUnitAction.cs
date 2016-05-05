248b41189cd657c1d4891d1e2530595ce28b4f6a
Karl Harnagy <kharnagy@inedo.com>
2016-05-05 16:00:27 -0400

    Added jUnit action.

---
+++ Legacy/JUnitAction.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Inedo.BuildMaster;
+using Inedo.BuildMaster.Extensibility.Actions.Testing;
+using Inedo.BuildMaster.Extensibility.Agents;
+using Inedo.BuildMaster.Web;
+using Inedo.Serialization;
+
+namespace Inedo.BuildMasterExtensions.JUnit
+{
+    [DisplayName("Execute jUnit Tests")]
+    [Description("Executes jUnit 4.x and later tests.")]
+    [CustomEditor(typeof(JUnitActionEditor))]
+    public sealed class JUnitAction : UnitTestActionBase
+    {
+        private StringBuilder standardOut = new StringBuilder();
+
+        /// <summary>
+        /// Gets or sets the path to java.
+        /// </summary>
+        [Persistent]
+        public string JavaPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search pattern to use for test classes.
+        /// </summary>
+        [Persistent]
+        public string SearchPattern { get; set; } = "*Test.class";
+
+        /// <summary>
+        /// Gets or sets the extension directories.
+        /// </summary>
+        /// <remarks>
+        /// Cross-compile against the specified extension directories. Directories is a semicolon-separated list of directories. Each JAR archive in the specified directories is searched for class files. 
+        /// </remarks>
+        [Persistent]
+        public string[] ExtensionDirectories { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Run jUnit Tests on {0} in {1}",
+                this.SearchPattern,
+                Util.CoalesceStr(this.OverriddenSourceDirectory, "default directory"));
+        }
+
+        protected override void RunTests()
+        {
+            //Output jars
+            var bmjPath = Path.Combine(
+                this.Context.Agent.GetService<IFileOperationsExecuter>().GetBaseWorkingDirectory(),
+                "ExtTemp",
+                "JUnit",
+                "BuildMaster.jar"
+            );
+
+            // Build file list
+            var testClasses = Directory.GetFiles(
+                this.Context.SourceDirectory,
+                this.SearchPattern,
+                SearchOption.AllDirectories);
+            for (int i = 0; i < testClasses.Length; i++)
+            {
+                if (testClasses[i].EndsWith(".class"))
+                    testClasses[i] = testClasses[i].Substring(0, testClasses[i].Length - ".class".Length);
+                testClasses[i] = testClasses[i]
+                    .Substring(this.Context.SourceDirectory.Length)
+                    .TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+                    .Replace(Path.DirectorySeparatorChar, '.')
+                    .Replace(Path.AltDirectorySeparatorChar, '.');
+            }
+
+            // Write to file
+            string FILE_testClasses = Path.GetTempFileName();
+            File.WriteAllLines(FILE_testClasses, testClasses);
+
+            // Capture Command Output
+            standardOut = new StringBuilder();
+            this.ExecuteCommandLine(
+                this.JavaPath,
+                string.Format(
+                    "-cp .;{0} -Djava.ext.dirs=\"{1}\" {2} \"@{3}\"",
+                    bmjPath,
+                    ExtensionDirectories == null ? null : string.Join(";", ExtensionDirectories),
+                    "inedo.buildmasterextensions.java.jUnitAction",
+                    FILE_testClasses),
+                this.Context.SourceDirectory);
+
+            // Load as XML
+            var testResults = new XmlDocument();
+            try { testResults.LoadXml(standardOut.ToString()); }
+            catch (Exception ex)
+            {
+                this.LogError("Unable to load test results: " + ex.Message);
+                this.LogDebug(standardOut.ToString());
+                return;
+            }
+
+            var testTime = DateTime.UtcNow;
+
+            // Log Results
+            foreach (XmlElement tr in testResults.SelectNodes("TestResults/TestResult"))
+            {
+                XmlNodeList failures = tr.SelectNodes("Failures/Failure");
+
+                int runCount = int.Parse(tr.Attributes["RunCount"].Value);
+                int failCount = failures.Count;
+                int ignoreCount = int.Parse(tr.Attributes["IgnoreCount"].Value);
+                int runTime = int.Parse(tr.Attributes["RunTime"].Value);
+                string testName = tr.Attributes["Class"].Value;
+
+                StringBuilder testResult = new StringBuilder();
+                if (runCount > 0)
+                {
+                    testResult.AppendLine("Tests Run: " + runCount.ToString());
+                    if (failCount == 0)
+                        testResult.AppendLine().AppendLine("No Failures.");
+                    else if (failCount == 1)
+                        testResult.AppendLine().AppendLine("1 Failure.");
+                    else
+                        testResult.AppendLine().AppendLine(failCount.ToString() + " Failures.");
+                }
+
+                foreach (XmlElement fail in failures)
+                {
+                    testResult.AppendLine();
+                    testResult.AppendLine();
+                    testResult.AppendLine("__" + fail.Attributes["TestHeader"].Value + "__");
+                    testResult.AppendLine("Exception: " + fail.Attributes["ExceptionClass"].Value);
+                    testResult.AppendLine("Message: " + fail.Attributes["Message"].Value);
+                }
+
+                var tout = tr.SelectSingleNode("TestOutput") as XmlElement;
+                if (tout != null)
+                {
+                    testResult.AppendLine();
+                    testResult.AppendLine();
+                    testResult.AppendLine("__Output__"); 
+                    foreach (XmlCDataSection cdata in tout.ChildNodes)
+                    {
+                        testResult.AppendLine(cdata.Value);
+                    }
+                }
+
+                this.RecordResult(
+                    testName, 
+                    failCount == 0, 
+                    testResult.ToString(),
+                    testTime, testTime.AddMilliseconds(runTime));
+                testTime = testTime.AddMilliseconds(runTime);
+            }
+        }
+
+        protected override void LogProcessOutputData(string data)
+        {
+            standardOut.AppendLine(data);
+        }
+    }
+}
---
+++ Legacy/JUnitActionEditor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
+using Inedo.BuildMaster.Extensibility.Actions;
+using Inedo.BuildMaster.Web.Controls;
+using Inedo.BuildMaster.Web.Controls.Extensions;
+using Inedo.Web.Controls;
+
+namespace Inedo.BuildMasterExtensions.JUnit
+{
+    internal sealed class JUnitActionEditor : ActionEditorBase
+    {
+        private ValidatingTextBox txtGroupName;
+        private SourceControlFileFolderPicker ctlJavaPath;
+        private ValidatingTextBox txtExtensionDirectories;
+        private ValidatingTextBox txtSearchPattern;
+
+        public override bool DisplaySourceDirectory => true;
+
+        public override void BindToForm(ActionBase extension)
+        {
+            var jUnit = (JUnitAction)extension;
+            if (jUnit.ExtensionDirectories != null)
+                txtExtensionDirectories.Text = string.Join(Environment.NewLine, jUnit.ExtensionDirectories);
+            ctlJavaPath.Text = jUnit.JavaPath;
+            txtSearchPattern.Text = jUnit.SearchPattern;
+            txtGroupName.Text = jUnit.GroupName;
+        }
+
+        public override ActionBase CreateFromForm()
+        {
+            return new JUnitAction
+            {
+                ExtensionDirectories = Regex.Split(this.txtExtensionDirectories.Text, "\r?\n"),
+                JavaPath = ctlJavaPath.Text,
+                SearchPattern = txtSearchPattern.Text,
+                GroupName = txtGroupName.Text
+            };
+        }
+
+        protected override void CreateChildControls()
+        {
+            this.txtGroupName = new ValidatingTextBox
+            {
+                Required = true,
+                Text = this.Plan.Deployable_Name
+            };
+
+            this.ctlJavaPath = new SourceControlFileFolderPicker { ServerId = this.ServerId };
+
+            this.txtExtensionDirectories = new ValidatingTextBox
+            {
+                TextMode = TextBoxMode.MultiLine,
+                Rows = 4
+            };
+
+            this.txtSearchPattern = new ValidatingTextBox
+            {
+                Text = (new JUnitAction()).SearchPattern
+            };
+
+            this.Controls.Add(
+                new SlimFormField("Test group:", this.txtGroupName),
+                new SlimFormField("Java path:", this.ctlJavaPath),
+                new SlimFormField("Extensions paths:", this.txtExtensionDirectories)
+                {
+                    HelpText = "The relative path of Java extensions. Note that jUnit 4.x must be in the default or one of these directories."
+                },
+                new SlimFormField("Search pattern:", this.txtSearchPattern)
+                {
+                    HelpText = "The file mask that indicates which classes are to be tested."
+                }
+            );
+        }
+    }
+}
